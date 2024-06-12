38b04a61d42ba6e8a3eb5da9fdc99eea9569c4d0
burak <rburaksaritas@gmail.com>
2024-06-12 12:57:11 +0300

    Added PriceData struct and modified fetchPrice method
    - Added PriceData struct with fields for current price, open price, high price, low price, previous close, and timestamp.
    - Modified the fetchPrice method in StockService to return a pointer to PriceData instead of just the current price.
    - Updated GetStockPrice method to encode the entire PriceData struct instead of just the price value.
    - Adjusted error handling in fetchPrice to accommodate the new PriceData structure.

--- internal/service/stock_service.go
+++ internal/service/stock_service.go
@@ -15,9 +15,17 @@ type StockService struct {
 	ApiKey string
 }
 
+type PriceData struct {
+	CurrentPrice  float64 `json:"c"`
+	OpenPrice     float64 `json:"o"`
+	HighPrice     float64 `json:"h"`
+	LowPrice      float64 `json:"l"`
+	PreviousClose float64 `json:"pc"`
+	Timestamp     int64   `json:"t"`
+}
+
 func NewStockService() *StockService {
 	apiKey := os.Getenv("FINNHUB_API_KEY")
-	log.Println("API Key:", os.Getenv("FINNHUB_API_KEY"))
 	if apiKey == "" {
 		log.Fatal("FINNHUB_API_KEY not set in .env file")
 	}
@@ -27,33 +35,31 @@ func NewStockService() *StockService {
 func (s *StockService) GetStockPrice(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	stockId := vars["stockId"]
-	price, err := s.fetchPrice(stockId)
+	priceData, err := s.fetchPrice(stockId)
 	if err != nil {
 		http.Error(w, "Failed to fetch stock price", http.StatusInternalServerError)
 		return
 	}
-	json.NewEncoder(w).Encode(map[string]float64{"price": price})
+	json.NewEncoder(w).Encode(priceData)
 }
 
-func (s *StockService) fetchPrice(stockId string) (float64, error) {
+func (s *StockService) fetchPrice(stockId string) (*PriceData, error) {
 	url := fmt.Sprintf("https://finnhub.io/api/v1/quote?symbol=%s&token=%s", stockId, s.ApiKey)
 	resp, err := http.Get(url)
 	if err != nil {
-		return 0, fmt.Errorf("error making request to Finnhub: %v", err)
+		return nil, fmt.Errorf("error making request to Finnhub: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return 0, fmt.Errorf("error reading response body: %v", err)
+		return nil, fmt.Errorf("error reading response body: %v", err)
 	}
 
-	var data struct {
-		CurrentPrice float64 `json:"c"`
-	}
+	var data PriceData
 	if err := json.Unmarshal(body, &data); err != nil {
-		return 0, fmt.Errorf("error parsing JSON response: %v", err)
+		return nil, fmt.Errorf("error parsing JSON response: %v", err)
 	}
 
-	return data.CurrentPrice, nil
+	return &data, nil
 }
