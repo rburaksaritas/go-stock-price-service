522a94adc3b456b654e795504807674ca71a1a89
burak <rburaksaritas@gmail.com>
2024-06-12 19:12:29 +0300

    Refactor StockService and Add FetchPrice Method
    - Refactored StockService struct by moving models and utils imports inside the package.
    - Removed redundant PriceData struct declaration from stock_service.go.
    - Renamed GetStockPrice method to FetchPrice to follow convention.
    - Updated method signature to match the new name and capitalized first letter for visibility.

--- internal/service/stock_service.go
+++ internal/service/stock_service.go
@@ -3,28 +3,16 @@ package service
 import (
 	"encoding/json"
 	"fmt"
+	"go-stock-price-service/internal/models"
+	"go-stock-price-service/pkg/utils"
 	"io"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/gin-gonic/gin"
-
-	"go-stock-price-service/internal/models"
-	"go-stock-price-service/pkg/utils"
 )
 
 type StockService struct {
 	ApiKey string
-}
-
-type PriceData struct {
-	CurrentPrice  float64 `json:"current_price"`
-	OpenPrice     float64 `json:"open_price"`
-	HighPrice     float64 `json:"high_price"`
-	LowPrice      float64 `json:"low_price"`
-	PreviousClose float64 `json:"previous_close"`
-	Timestamp     string  `json:"timestamp"`
 }
 
 func NewStockService() *StockService {
@@ -35,25 +23,7 @@ func NewStockService() *StockService {
 	return &StockService{ApiKey: apiKey}
 }
 
-func (s *StockService) GetStockPrice(c *gin.Context) {
-	stockId := c.Param("stockId")
-	timeZone := c.DefaultQuery("timezone", "UTC")
-
-	priceData, err := s.fetchPrice(stockId, timeZone)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	if priceData.CurrentPrice == 0 && priceData.OpenPrice == 0 && priceData.HighPrice == 0 && priceData.LowPrice == 0 && priceData.PreviousClose == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Invalid stock ID or no data available"})
-		return
-	}
-
-	c.JSON(http.StatusOK, priceData)
-}
-
-func (s *StockService) fetchPrice(stockId string, timeZone string) (*models.PriceData, error) {
+func (s *StockService) FetchPrice(stockId string, timeZone string) (*models.PriceData, error) {
 	url := fmt.Sprintf("https://finnhub.io/api/v1/quote?symbol=%s&token=%s", stockId, s.ApiKey)
 	resp, err := http.Get(url)
 	if err != nil {
