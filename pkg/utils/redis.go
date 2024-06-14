d823e614e87f594e600d3ecd54a1d9cccea8ad50
burak <rburaksaritas@gmail.com>
2024-06-14 13:32:27 +0300

    Create Redis utility package with RedisClient struct
    - New file created: pkg/utils/redis.go
    - Defines a RedisClient struct with methods for setting and getting data in Redis using go-redis library.
    - Implements Set method to store data in Redis with expiration and Get method to retrieve data and unmarshal into specified struct.

---
+++ pkg/utils/redis.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type RedisClient struct {
+	client *redis.Client
+	ctx    context.Context
+}
+
+func NewRedisClient(addr, password string, db int) *RedisClient {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	})
+	ctx := context.Background()
+	return &RedisClient{client: rdb, ctx: ctx}
+}
+
+func (r *RedisClient) Set(key string, value interface{}, expiration time.Duration) error {
+	jsonData, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	return r.client.Set(r.ctx, key, jsonData, expiration).Err()
+}
+
+func (r *RedisClient) Get(key string, dest interface{}) error {
+	result, err := r.client.Get(r.ctx, key).Result()
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal([]byte(result), dest)
+}
