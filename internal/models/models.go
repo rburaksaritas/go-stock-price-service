4aec4b0d5310c4f95d6f45cf36cf88e9c36ebdc7
burak <rburaksaritas@gmail.com>
2024-06-14 11:14:00 +0300

    Add Provider Field and AlphaVantagePriceData Struct
    - Added a new field 'Provider' to the PriceData struct to include provider information.
    - Defined a new struct AlphaVantagePriceData with nested GlobalQuote struct, containing symbol, open, high, low, price, and previous close fields.

--- internal/models/models.go
+++ internal/models/models.go
@@ -7,6 +7,7 @@ type PriceData struct {
 	LowPrice      float64 `json:"low_price"`
 	PreviousClose float64 `json:"previous_close"`
 	Timestamp     string  `json:"timestamp"`
+	Provider      string  `json:"provider"`
 }
 
 type RawDataFinnhub struct {
@@ -16,4 +17,15 @@ type RawDataFinnhub struct {
 	LowPrice      float64 `json:"l"`
 	PreviousClose float64 `json:"pc"`
 	Timestamp     int64   `json:"t"`
+}
+
+type AlphaVantagePriceData struct {
+	GlobalQuote struct {
+		Symbol        string  `json:"01. symbol"`
+		Open          float64 `json:"02. open,string"`
+		High          float64 `json:"03. high,string"`
+		Low           float64 `json:"04. low,string"`
+		Price         float64 `json:"05. price,string"`
+		PreviousClose float64 `json:"08. previous close,string"`
+	} `json:"Global Quote"`
 }
